6a223dc07846420c3898cd47776cb3fb45199343
suhartx <suhartx@opendeusto.es>
2022-09-01 09:17:54 +0200

    agregados pruebas de integracion

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-26T09:09:50+0100",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
+    date = "2022-08-31T18:05:45+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.16 (Ubuntu)"
 )
 @Component
 public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
